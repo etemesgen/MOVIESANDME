93e918c129abde2a8c535e32b750fe7c33099e56
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-08-01 01:07:38 +0200

    Learned how to create a vue/ component

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import Search from './Components/Search'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.firstText]}>je peut developper meme facebook </Text>
-      <Button
-        buttonStyle={styles.button}
-        title="Cliquez moi..."
-        onPress={() => Alert.alert('Mon gars Edo ;) ça c\'\est ta passion')}
-        color="#FFC501"
-      //  backgroundColor="#000000"
-      />
-      <StatusBar style="auto" />
-    </View>
+    <Search/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  firstText: {
-    color: '#FFC501',
-    fontSize: 30,
-    backgroundColor: '#BC0BFF',
-  },
-  button: {
-    backgroundColor: '#000000', 
-    borderColor: '#000000',
-    borderWidth: 5,
-    borderRadius: 15
-  },
-});
