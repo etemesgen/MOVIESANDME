1915170696d13686bf7fb65ad003ba888cf94abf
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-08-10 03:49:09 +0200

    Learned some on styling, flexbox

--- Components/Search.js
+++ Components/Search.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import {View, Button, TextInput} from 'react-native'
+import {StyleSheet, View, Button, TextInput} from 'react-native'
 
 class Search extends React.Component{
     render(){
         return(
-            <View>
-                <TextInput placeholder="Titre du film"/>
-                <Button title="Rechercher" onPress={() => {}}/>
+            <View style={styles.main_container}>
+                <TextInput style={styles.textinput} placeholder="Titre du film"/>
+                <Button style={{ height: 50}} title="Rechercher" onPress={() => {}}/>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    main_container: {
+        marginTop: 35, 
+        flex: 1
+    },
+    textinput: {
+        marginLeft: 5, 
+        marginRight: 5, 
+        height: 50, 
+        borderColor: '#000000', 
+        borderWidth: 1, 
+        paddingLeft: 5
+    }
+})
 
 export default Search
