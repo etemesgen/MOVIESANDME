fcf56b91e0d2ef6f231d13cf94621e21ed5a59a4
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-08-19 19:06:30 +0200

    Learnt props and how to use them

--- Components/Search.js
+++ Components/Search.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import {StyleSheet, View, Button, TextInput} from 'react-native'
+import {StyleSheet, View, Button, TextInput, FlatList, Text} from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
 
 class Search extends React.Component{
     render(){
         return(
             <View style={styles.main_container}>
                 <TextInput style={styles.textinput} placeholder="Titre du film"/>
-                <Button style={{ height: 50}} title="Rechercher" onPress={() => {}}/>
+                <Button title="Rechercher" onPress={() => {}}/>
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem film={item}/>}
+                />
             </View>
         )
     }
@@ -14,7 +21,7 @@ class Search extends React.Component{
 
 const styles = StyleSheet.create({
     main_container: {
-        marginTop: 35, 
+        marginTop: 50, 
         flex: 1
     },
     textinput: {
