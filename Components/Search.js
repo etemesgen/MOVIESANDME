64b5bf59076bb019201555c59be1b363190b59cb
Edomiyas <temesgen.edomiyas@yahoo.com>
2021-09-08 17:36:30 +0200

    Added some functionalities in the app

--- Components/Search.js
+++ Components/Search.js
@@ -1,7 +1,7 @@
 // Components/Search.js
 
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
@@ -11,30 +11,47 @@ class Search extends React.Component {
     super(props)
     this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
     this.state = {
-      films: []
+      films: [],
+      isLoading: false
     }
   }
 
   _loadFilms() {
+    this.setState({ isLoading: true })
     if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
       getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-          this.setState({ films: data.results })
+          this.setState({
+            films: data.results, 
+            isLoading: false 
+          })
       })
     }
   }
+
+  _displayLoading(){
+    if(this.state.isLoading){
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large'/>
+        </View>
+      )
+    }
+  }
+
 
   _searchTextInputChanged(text) {
     this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
   }
 
   render() {
-    console.log("RENDER")
+    console.log(this.state.isLoading);
     return (
       <View style={styles.main_container}>
         <TextInput
           style={styles.textinput}
           placeholder='Titre du film'
           onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._loadFilms()}
         />
         <Button title='Rechercher' onPress={() => this._loadFilms()}/>
         <FlatList
@@ -42,6 +59,7 @@ class Search extends React.Component {
           keyExtractor={(item) => item.id.toString()}
           renderItem={({item}) => <FilmItem film={item}/>}
         />
+        {this._displayLoading()}
       </View>
     )
   }
@@ -59,6 +77,15 @@ const styles = StyleSheet.create({
         borderColor: '#000000', 
         borderWidth: 1, 
         paddingLeft: 5
+    },
+    loading_container: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 100,
+      bottom: 0,
+      alignItems: 'center',
+      justifyContent: 'center'
     }
 })
 
